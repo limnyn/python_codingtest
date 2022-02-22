3a1cb5ce742c0afe65623b064ff90b3008197781
MnynLi <hogandi001@gmail.com>
2022-02-22 22:45:17 +0900

    5-11 maze clear!

--- codingtest/5-11_미로탈출.py
+++ codingtest/5-11_미로탈출.py
@@ -31,3 +31,47 @@
 #     10
 
 
+
+n, m = map(int, input().split())
+
+maplist = []
+for _ in range(n):
+    maplist.append(list(map(int, input())))
+
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+from collections import deque
+
+
+    
+def bfs(x,y):
+    # queue 구현을 위해 deque 라이브러리 사용
+    queue = deque()
+    queue.append((x,y))
+    # 큐가 빌 때까지 반복
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 네 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로찾기 공간을 벗어난 경우 무시
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if maplist[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if maplist[nx][ny] == 1:
+                maplist[nx][ny] = maplist[x][y] + 1
+                queue.append((nx,ny))
+            # 가장 오른쪽 아래까지의 최단 거리 반환
+    return maplist[n-1][m-1]
+
+print(bfs(0,0))
+
+
+
+            
