cf654a9456c3140708b3dcef7d6d449ee1d350de
limnyn <hogandi001@gmail.com>
2023-01-20 23:59:38 +0900

    solution 1557

--- 백준/ex/1557_제곱ㄴㄴ.py
+++ 백준/ex/1557_제곱ㄴㄴ.py
@@ -32,30 +32,48 @@
 # 2030745
 
 
-import sys
 
-maxNum = int(input())
-max2 = int(1.7*maxNum)
-result = []
-array = [False]*(max2)
-for i in range (2, int(max2**0.5)+1):
-    square = i**2
-    for j in range(0, max2, square):
-        if not array[j]:
-            array[j] = True
-    result.append(square)
-# print(result)
-for i in range(max2):
-    if array[i] == True:
-        array[i] = 0
-    else:
-        array[i] = i
-array = [i for i in array if i != 0]
-print(array[maxNum-1])
 
-# i = 2
-# while(maxNum > 0):
-#     if(i,int(max2))
+
 
 
 # 1000000000
+
+# 메모리 초과
+# maxNum = int(input())
+# max2 = int(1.7*maxNum)
+# result = []
+# array = [False]*(max2)
+# for i in range (2, int(max2**0.5)+1):
+#     square = i**2
+#     for j in range(0, max2, square):
+#         if not array[j]:
+#             array[j] = True
+#     result.append(square)
+# # print(result)
+# for i in range(max2):
+#     if array[i] == True:
+#         array[i] = 0
+#     else:
+#         array[i] = i
+# array = [i for i in array if i != 0]
+# print(array[maxNum-1])
+
+
+4 9 16 25 36 49 64 81 100
+4+9-4*9
+4+9+16-(4*9+4*16+9*16)+4*9*16
+
+
+만약 1000까지 소수가 아닌 갯수를 찾을 때
+2*3*5*7*11  = 2310,
+1000//2310 == 0, 따라서 1000 아래 중에 2*3*5*7*11의 배수는 없다.
+따라서 최대로 뽑는 개수는 4개까지 이므로 
+
+정답 ==
+1000 - (2,3,5,7)의 배수 - 2개의 공배수, + 3개의 공배수 - +(2*3*5*7)의 공배수
+
+필요한 함수 -> 
+    1. m**0.5의 소수 찾기 
+    2. 곱이 m을 초과하지 않는 횟수 찾기
+    
