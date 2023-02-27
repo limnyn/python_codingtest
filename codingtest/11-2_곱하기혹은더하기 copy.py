6f78892c080c5148bdcae3cc69ae262569a31692
limnyn <hogandi001@gmail.com>
2023-02-27 20:12:55 +0900

    python_codingtest_11-3

--- codingtest/11-2_곱하기혹은더하기 copy.py
+++ codingtest/11-2_곱하기혹은더하기 copy.py
@@ -1,42 +1,33 @@
-# 0과 1로만 이루어진 문자열 S
-# S에 있는 모든 숫자를 전부 같게 만들려고 합니다. 
-# S에서 연속된 하나 이상의 숫자를 잡고 모두 뒤집습니다.
-# 뒤집는것은  1을 0으로, 0을 1로 바꾸는 것입니다.
-# S = 0001100 일 때
-    # 4번째 문자부터 5번째 문자까지 뒤집으면 1111111이 되어서 한번에 모두 같은 숫자로 만들 수 있습니다.
-
-# 문자열 S가 주어졌을 때, 행동의 최소 횟수
+# 숫자로만 이루어진 문자열 S가 주어졌을 때,
+# 각 숫자 사이에 x와 + 연산자를 넣어 결과적으로 만들 어 질 수 있는 가장 큰 수를 구하는 프로그램
+# 단 모든 연산은  x가 +보다 먼저 연산되는 것이 아닌 
+# 왼쪽에서 오른쪽으로 한단계씩 연산하는 순서입니다.
 
 # 입력 조건
-#     첫째 줄에 0과 1로만 이루어진 문자열 S가 주어집니다. S의 길이는 100만 보다 작습니다.
+#     첫째 줄에 여러 개의 숫자로 구성된 하나의 문자열 S가 주어집니다. (1 <= S의 길이 <= 20)
 
 # 출력 조건
-#     첫째줄에 다솜이가 해야하는 행동의 최소 횟수를 출력합니다.
+#     첫째 줄에 만들어질 수 있는 가장 큰 수를 출력합니다.
 
 # 입력 예시
-#     00011000
+#     02984
 
 # 출력 예시
-#     1
+#     576
 
 S = input()
 
-def act(S):
-    # blob => list which have strings made of same nums
-    blob = []
-    start = 0
-    prev = S[0]
-    for i, s in enumerate(S):
-        if s != prev:
-            blob.append(S[start:i])
-            start =  i
-        prev = s
-        
-    blob.append(S[start:len(S)])
-    
-    return len(blob)//2
+def solution(S):
+    result = int(S[0])
+    for i in range(1, len(S)):
+        if int(S[i-1]) <= 1:
+            result += int(S[i])
+        else:
+            result *=  int(S[i])
+
+    return result
 
     
             
 
-print(act(S))
+print(solution(S))
