a81b742020baac31249788b54a70ced8876847d7
limnyn <hogandi001@gmail.com>
2023-03-09 21:14:41 +0900

    python_codingtest_12-14

--- codingtest/12-13_치킨배달.py
+++ codingtest/12-13_치킨배달.py
@@ -2,8 +2,6 @@
 
 N, M = map(int, input().split())
 
-
-city = []
 houses = []
 chickens = []
 for row in range(N):
@@ -13,43 +11,26 @@ for row in range(N):
             houses.append([row, col, 1e9])
         if spot == 2:
             chickens.append([row,col])                   
-    city.append(line)
 
 
 
 def calc_dist(houses, chickens):
-
     chicken_dist = 0
     for house in houses:
-         
         house[2] = 1e9
-        for chicken in chickens:
-            x, y, house_to_chicken = house[0], house[1], house[2]
-            cx, cy = chicken[0], chicken[1]
+        for cx, cy in chickens:
+            x, y = house[0], house[1]
             dist = abs(x - cx) + abs(y - cy)
-            
-            if dist < house_to_chicken:
-                house[2] = dist
-                
+            house[2] = min(dist, house[2])
         chicken_dist += house[2]
-    
-
     return chicken_dist
 
             
 # 치킨집 갯수중에 M개를뽑는 조합 
 import itertools
-combination = itertools.combinations(chickens, M)
+combination = list(itertools.combinations(chickens, M))
 result = 1e9
 for comb in combination:
-    comb_chicken = []
-    for i in comb:
-        # print("i = ",i)
-        comb_chicken.append(i)
-    dist = calc_dist(houses,comb_chicken)
-    if dist < result:
-        result = dist
-
-
+    result = min(result, calc_dist(houses,comb))
 
 print(result)
