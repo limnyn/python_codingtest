cd8519899fff219823d68a573a853a52a0e5c4b5
limnyn <hogandi001@gmail.com>
2023-09-12 21:39:03 +0900

    programmers 154538 solved

---
+++ 프로그래머스/154538_숫자변환하기.py
@@ -0,0 +1,34 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154538
+
+
+from collections import deque
+
+
+def solution(x, y, n):
+    dq = deque([])
+
+    dq.append((y, 0))
+
+    while dq:
+        y, count = dq.popleft()
+
+        if y == x:
+            return count
+        if y < x:
+            continue
+
+        count += 1
+        if y % 3 == 0:
+            dq.append((y // 3, count))
+
+        if y % 2 == 0:
+            dq.append((y // 2, count))
+
+        dq.append((y - n, count))
+
+    return -1
+
+
+print(solution(10, 40, 5))
+print(solution(10, 40, 30))
+print(solution(2, 5, 4))
