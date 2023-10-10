27b1ad2e2f61b02d1956245ca415eb121e84d265
Limnyn <hogandi001@gmail.com>
2023-10-10 13:01:01 +0900

    programmers 138476 solved

---
+++ 프로그래머스/138476_귤_고르기.py
@@ -0,0 +1,18 @@
+def solution(k, tangerine):
+    from collections import Counter
+
+    counter = Counter(tangerine).most_common()
+    # [(3, 2), (2, 2), (5, 2), (1, 1), (4, 1)] 이런 형태로 반환
+
+    # k에서 하나씩 제외
+    answer = 0
+    for num, count in counter:
+        k -= count
+        answer += 1
+        if k <= 0:
+            return answer
+
+
+k = 6
+tangerine = [1, 3, 2, 5, 4, 5, 2, 3]
+print(solution(k, tangerine))
