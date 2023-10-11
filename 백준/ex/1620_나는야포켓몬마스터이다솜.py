0096bd47e308d55472901dcf02a9b9f4d18eeff0
Limnyn <hogandi001@gmail.com>
2023-10-11 17:49:19 +0900

    boj 1620

---
+++ 백준/ex/1620_나는야포켓몬마스터이다솜.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/1620
+
+n, m = map(int, (input().split()))
+
+poketmon_name = {}
+poketmon_list = []
+for i in range(n):
+    pmon = input()
+    poketmon_name[pmon] = i + 1
+    poketmon_list[i + 1] = pmon
+
+asks = []
+for _ in range(m):
+    asks.append(input())
+
+for ask in asks:
+    if ask.isdigit():
+        print(poketmon_list[int(ask)])
+    else:
+        print(poketmon_name[ask])
