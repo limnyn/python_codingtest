119788be7c6c39a2282da7535c890aefad3d6008
Limnyn <hogandi001@gmail.com>
2023-10-12 16:06:03 +0900

    boj 2606

---
+++ 백준/ex/2606_바이러스.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/2606
+
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n)]
+visited = [0] * n
+for i in range(m):
+    s, e = map(int, input().split())
+    graph[s - 1].append(e - 1)
+    graph[e - 1].append(s - 1)
+
+
+from collections import deque
+
+dq = deque()
+dq.append(0)
+
+while dq:
+    start = dq.popleft()
+    for edge in graph[start]:
+        if not visited[edge]:
+            visited[edge] = 1
+            dq.append(edge)
+
+print(sum(visited) - 1)
