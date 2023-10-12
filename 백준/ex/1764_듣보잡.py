bbaf157d97dfee26c0476e8d0ffbdc7fa88c83d0
Limnyn <hogandi001@gmail.com>
2023-10-12 13:39:23 +0900

    boj 1764

---
+++ 백준/ex/1764_듣보잡.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/1764
+# import sys
+
+# input = sys.stdin.readline
+n, m = map(int, input().split())
+unh = []
+uns = []
+for i in range(n):
+    unh.append(input())
+for i in range(m):
+    uns.append(input())
+s_unh = set(unh)
+s_uns = set(uns)
+
+answer = list(s_unh.intersection(s_uns))
+
+print(len(answer))
+for a in sorted(answer):
+    print(a)
