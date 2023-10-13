e61419f4d31f82b20cc41f697691ad3f754b3c50
Limnyn <hogandi001@gmail.com>
2023-10-13 16:03:26 +0900

    swea 1216

---
+++ SWEA/D3/1216_회문2.py
@@ -0,0 +1,33 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AV15Khn6AN0CFAYD&categoryId=AV15Khn6AN0CFAYD&categoryType=CODE&problemTitle=&orderBy=INQUERY_COUNT&selectCodeLang=ALL&select-1=3&pageSize=10&pageIndex=1
+
+
+# 가장 긴 팰린드롬 반환
+def longest_palindrome(grid):
+    result = 0
+    for n in range(100, 0, -1):
+        for line in grid:
+            for c in range(100 - n + 1):
+                txt = line[c : c + n]
+                txt_r = txt[::-1]
+                if txt == txt_r:
+                    if result < n:
+                        result = n
+                        return result
+
+
+for t_c in range(10):
+    t_c_num = int(input())
+    result = 0
+    grid_r = []
+    grid_c = [[] for _ in range(100)]
+
+    for row in range(100):
+        line = input()
+        grid_r.append([c for c in line])
+        for i, c in enumerate(line):
+            grid_c[i].append(c)
+
+    # 가로 세로 그리드 중 가장 긴 펠린드롬을 찾는다
+    result = max(longest_palindrome(grid) for grid in [grid_r, grid_c])
+
+    print("#" + str(t_c + 1) + " " + str(result))
