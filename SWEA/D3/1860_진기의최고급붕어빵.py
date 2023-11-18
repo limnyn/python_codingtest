53b5a992fce258e8785b77bf39bb1b9d42810788
Limnyn <hogandi001@gmail.com>
2023-11-18 14:17:01 +0900

    swea 1860

---
+++ SWEA/D3/1860_진기의최고급붕어빵.py
@@ -0,0 +1,29 @@
+
+
+for t_c in range(1, int(input())+1):
+    n, m, k = map(int, input().split())
+    #손님, m초동안 k개 붕어빵
+    customers = list(map(int, input().split()))
+    customers.sort()
+
+    result = "Possible"
+    for i in range(n):
+        boong = (customers[i]//m)*k - (i+1)
+        if boong <= 0:
+            result = "Impossible"
+            break
+
+    print(f'#{t_c} {result}')
+        
+        
+
+
+
+
+
+# time = m
+# count = k
+# for c in customers:
+#     if c < time:
+#         count -= 1
+        
