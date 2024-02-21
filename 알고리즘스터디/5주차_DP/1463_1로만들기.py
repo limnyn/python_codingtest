4a32454e0fabf44cc812ed03cab92ff00caceacc
limnyn <hogandi001@gmail.com>
2024-02-21 23:46:20 +0900

    boj 1463

--- 알고리즘스터디/5주차_DP/1463_1로만들기.py
+++ 알고리즘스터디/5주차_DP/1463_1로만들기.py
@@ -6,7 +6,7 @@ dp[n] = min(dp[i//3], dp[i//2], dp[i-1])
 n = int(input())
 
 dp = [0]*1000001
-dp[0] = 1
+
 dp[1] = 0
 dp[2] = 1
 dp[3] = 1
@@ -17,7 +17,6 @@ for i in range(4, n+1):
     if i % 2 == 0:
         candi.append(dp[i//2])
     dp[i] = min(candi) + 1
-    if i == n:
-        break
+
 
 print(dp[n])
