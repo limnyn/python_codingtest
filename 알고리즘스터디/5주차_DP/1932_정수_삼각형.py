aeb4810bb42797c0ce0e4b5c576fbdf802ed298f
limnyn <hogandi001@gmail.com>
2024-02-23 23:56:42 +0900

    boj 1932

--- 알고리즘스터디/5주차_DP/1932_정수_삼각형.py
+++ 알고리즘스터디/5주차_DP/1932_정수_삼각형.py
@@ -1,3 +1,35 @@
+# 180ms -> 140ms 로 이전코드 대비 개선
+
+n = int(input())
+dp_before = []
+
+for r in range(1, n+1):
+    
+    line = list(map(int, input().split()))
+    dp_now = []
+    if n == 1:
+        print(line[0])
+        exit()
+    if r == 1:
+        dp_before = line
+        continue
+
+    for c in range(r):
+        if c == 0:
+            dp_now.append(line[0] + dp_before[0])
+        elif c == r-1:
+            dp_now.append(line[c] + dp_before[c-1])
+        else:
+            dp_now.append(
+                line[c] + max(dp_before[c-1], dp_before[c])
+            )
+    dp_before = dp_now
+    
+print(max(dp_now))
+
+
+# 개선 전 코드
+"""
 n = int(input())
 triangle=[]
 for _ in range(n):
@@ -26,10 +58,4 @@ for r, tri in enumerate(triangle):
 print(max(dp[n-1]))
         
 
-
-
-    
-        
-
-# for tri in dp:
-    # print(tri)
+"""
