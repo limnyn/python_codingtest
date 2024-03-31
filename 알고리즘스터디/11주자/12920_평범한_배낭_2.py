3a8c94160d65281a69fa7164c0782405a1b4db5a
limnyn <hogandi001@gmail.com>
2024-03-31 23:27:24 +0900

    boj 12920

---
+++ 알고리즘스터디/11주자/12920_평범한_배낭_2.py
@@ -0,0 +1,68 @@
+# https://www.acmicpc.net/problem/12920
+
+
+'''
+산다 안산다 2진수
+    비용이 A인 특정 물건이 5개 있을때 사는 경우
+        000 ~ 100
+        2진수 3비트로 물건을 사는 수를 포함 가능하다
+        따라서 비용이 A, A*2, A*3인 아이템이 있는 경우와 동일하다.
+        그러므로 c개 있을때는 물건을 c보다 작은 2진수 갯수로 구한다
+        
+        ex) c가 5면 3 c가 15면 4
+
+        
+    
+    해당 계산을 하기 위해서는?
+    
+    1 <= C <= 10000
+    
+    2^14를 넘지 않는다,
+    
+    for문을 통해 갯수 c를 표현 가능하도록 해당 수의 2진수 변환 시 자리 수 만큼
+    물건을 해당 진수의 배수만큼 더 추가해준다.
+
+
+    메모리 초과를 막기 위해
+        5개인 경우 1, 2, 2개인 경우를 추가해준다
+
+'''
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+w = []
+v = []
+for _ in range(n):
+    weight, value, count = map(int,input().split())
+    
+    bit_len = 0
+    sum_bit = 0 
+    while True:
+        sum_bit += 2**bit_len
+        if sum_bit <= count:
+            w.append(weight*2**bit_len)
+            v.append(value*2**bit_len)
+            bit_len += 1
+        else:
+            if count - (sum_bit - 2 ** bit_len) != 0:
+                w.append(weight*(count-(sum_bit-2**bit_len)))
+                v.append(value*(count-(sum_bit-2**bit_len)))
+            break             
+
+
+
+dp = [[0] * (m+1) for _ in range(len(w)+1)]
+bf_dp = [0] * (m+1)
+for r in range(1, len(w)+1):
+    af_dp = [0]*(m+1)
+    for c in range(1, m+1):
+        if c-w[r-1] >= 0:
+            af_dp[c] = max(bf_dp[c], bf_dp[c-w[r-1]]+v[r-1])
+        else:
+            af_dp[c] = bf_dp[c]
+    bf_dp = af_dp
+
+    
+
+print(af_dp[-1])
--- 알고수업/18일차_문제풀이/프로세서_연결하기.py
+++ 알고수업/18일차_문제풀이/프로세서_연결하기.py
@@ -10,16 +10,120 @@
     
     접근 방법
         1. Core를 뽑는 멱집합
-        N과 Core의 개수가 적고, '최대한 많은 core에 전원 연결' 조건에 따라서
-        "백트래킹"을 이용할 수 있을 거 같다
+            
+            N과 Core의 개수가 적고, '최대한 많은 core에 전원 연결' 조건에 따라서
+            "백트래킹"을 이용할 수 있을 거 같다
 
-        "최대한 많은 core에 전원 연결" 조건에 따라
-        만약 현재 탐색이 최대 core갯수보다 작다면 가지치기를 통해 탐색을 종료하고
-        최대 core 갯수와 같거나 많다면, 계속 탐색하며 갱신하는 방식을 사용하면 될 것 같다
-
+            N의 최대값 12
+            
+            각 코어에 대해 방향을 5개(4방향 + 연결 안하는 경우)를 선택한다면
+                5^12 경우
+                이 중 백트래킹을 통해 겹치는 경우 탐색을 하지 않는다면 줄일 수 있을 것 같다.
+            
+        2. 조합에 대해 전선의 길이를 구하는 경우
+            각 조합의 방향에 따라 방문처리하며 전진을 한다.
+            만약 전진 방향이 방문되어 있으면 return -1
+            그 외에는 전체 전선 길이를 반환한다.
     
 '''
+dr = [-1, 0, 1, 0, 0]
+dc = [0, 1, 0, -1, 0]
 
-def backtracking():
-    pass
+def calc(combi):
+    visited = [[0]*N for _ in range(N)]
+    wire_len = 0
+    core_cnt = 0
+    for idx in range(CORE_LEN):
+    
+        spot_r, spot_c = core_spot[idx]
+        visited[spot_r][spot_c] = 1
 
+    for idx in range(CORE_LEN):
+        nr, nc = core_spot[idx]
+        dir = combi[idx]
+        if dir == 4:
+            if nr == 0 or nr == N-1 or nc == 0 or nc == N-1:
+                core_cnt += 1
+            continue
+        if dir < 4:
+            core_cnt += 1
+
+        # if nr == 0 or nr == N-1 or nc == 0 or nc == N-1:
+            
+            nr = nr + dr[dir]
+            nc = nc + dc[dir]
+            while 0 <= nr < N and 0 <= nc < N:
+                if visited[nr][nc] == 1:
+                    return -1, -1
+                
+                visited[nr][nc] = 1
+                wire_len += 1
+                nr = nr + dr[dir]
+                nc = nc + dc[dir]
+
+    return core_cnt, wire_len
+        
+
+
+    
+
+def backtracking(combi):
+    global max_core_cnt, min_wire_len
+
+    if len(combi) == CORE_LEN:
+        # print(combi)
+        core_cnt, wire_len = calc(combi)
+        if core_cnt != -1:
+            if max_core_cnt == 0:
+                max_core_cnt = core_cnt
+
+            if core_cnt == max_core_cnt:
+                # max_core_cnt = core_cnt
+                min_wire_len = min(wire_len, min_wire_len)
+            else:
+                return False
+        
+
+
+
+    else:
+        for dir in range(5):
+            backtracking(combi + [dir])
+        
+
+
+if __name__ == "__main__":
+    for t_c in range(1, int(input())+1):
+        grid = []
+        max_core_cnt = 0
+        min_wire_len = 1e9
+
+        N = int(input())    
+        core_spot = []
+        for r in range(N):
+            line = list(map(int, input().split()))
+            for c in range(N):
+                if line[c] == 1:
+                    core_spot.append((r,c))
+            grid.append(line)
+        CORE_LEN = len(core_spot)
+        backtracking([])
+        print(f"#{t_c} {min_wire_len}")
+
+            
+    
+
+
+
+
+'''
+1
+7    
+0 0 1 0 0 0 0
+0 0 1 0 0 0 0
+0 0 0 0 0 1 0
+0 0 0 0 0 0 0
+1 1 0 1 0 0 0
+0 1 0 0 0 0 0
+0 0 0 0 0 0 0
+'''
