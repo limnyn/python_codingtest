fa422a4dd54f064807ec7447128aa74804e0b14e
limnyn <hogandi001@gmail.com>
2024-06-20 22:02:08 +0900

    boj 9466 fix

--- 백준/ex/9466_텀_프로젝트.py
+++ 백준/ex/9466_텀_프로젝트.py
@@ -11,7 +11,7 @@
 [접근]
     1. 각 시작점 별로 visited를 번호로 매겨 같은 싸이클에 대해 중복순회를 돌지 않게 한다
     2. 방문한 노드가 나올 때 까지 스택을 통해 깊이 탐색을 한다
-    3. pop 하며 마지막 노드가 이전 노드와 일치할 때 까지 스택에서 pop 한다
+    3. 마지막 노드가 이전 노드와 일치할 때 까지 스택에서 pop 한다
         3-1. 이때까지 반복되는 동안은 Group이기 때문에 Group 내부에 포함되었다고 체크해준다 - grouped
     4. group에 포함되지 않은 노드들을 count 해서 더한다.
 
@@ -30,13 +30,13 @@ if __name__ == "__main__":
         idx = 0
 
         while idx < n:
-            node = idx
+            node = idx # 1.
             if visited[node] != -1:
                 idx += 1
                 continue
 
             stack = []
-            while visited[node] == -1:
+            while visited[node] == -1: # 2.
                 stack.append(node)
                 visited[node] = idx
                 node = arr[node]
@@ -44,7 +44,7 @@ if __name__ == "__main__":
             if visited[node] == idx:
                 grouped[node] = True
                 
-                while stack:
+                while stack: # 3.
                     before = stack.pop()
                     if before == node:
                         break
@@ -52,7 +52,7 @@ if __name__ == "__main__":
             
             idx += 1
         
-        result = 0
+        result = 0 # 4.
         for g in grouped:
             if not g:
                 result += 1
